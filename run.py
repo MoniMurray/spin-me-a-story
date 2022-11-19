86800deb41370731e53664c6e2a02e7e0776cd19
Monica Murray <Monica.rickerby@gmail.com>
2022-11-19 12:40:30 +0000

    Add while loop to run until correct input received from user

--- run.py
+++ run.py
@@ -7,15 +7,36 @@ def greetings():
     user_name = input("Hello!  What is your name?\n")
     return user_name
 
-def new_story(user_name):
-    start_story = input(f"Would you like to write a story, {users_name}?\n Select 'y for yes, or 'n' to exit.  y/n \n")
-    if start_story == 'y':
-        print("Once Upon a Time...")
-    elif start_story == 'n':
-        print(f"Goodbye, {users_name}")
-    else: 
-        print("Please select either y/n.\n")
+def new_story(users_name):
+    while True:
+        start_story = input(f"Would you like to write a story, {users_name}?\nSelect 'y for yes, or 'n' to exit.\ny/n \n")
+
+        if start_story == 'y':
+            print("Once Upon a Time...")
+            break
+        elif start_story == 'n':
+            print(f"Goodbye, {users_name}")
+            break
+        
+        elif user_continue(start_story, users_name):
+            print("OKAY")
+            break
+        
     return start_story
 
-users_name = greetings()
-new_story(users_name)
+def user_continue(start_story, users_name):
+    try:
+        if start_story != 'y':
+            if start_story != 'n':
+                raise ValueError(f"Only y/n can be accepted, you typed {start_story}")
+    except ValueError as e:
+        print(f"ValueError: Please select either y/n.\n")
+        return False
+    
+    return True
+
+def main():
+    users_name = greetings()
+    start_new_story = new_story(users_name)
+
+main()
