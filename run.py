0d84a30fe318f43ed5137503c6729f7666cc43e3
Monica Murray <Monica.rickerby@gmail.com>
2022-11-19 11:11:33 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def greetings():
+    user_name = input("Hello!  What is your name?\n")
+    print("Hello " + user_name)
+
+greetings()
