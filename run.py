f64fdbbef015e0cca3b1594c6abc41878222b0c2
Monica Murray <Monica.rickerby@gmail.com>
2022-11-19 11:32:13 +0000

    Add user's name with request for user input, return user name in invitation to continue, with prompt for correct input if not as requested

--- run.py
+++ run.py
@@ -5,6 +5,17 @@ import random
 
 def greetings():
     user_name = input("Hello!  What is your name?\n")
-    print("Hello " + user_name)
+    return user_name
 
-greetings()
+def new_story(user_name):
+    start_story = input(f"Would you like to write a story, {users_name}?\n Select 'y for yes, or 'n' to exit.  y/n \n")
+    if start_story == 'y':
+        print("Once Upon a Time...")
+    elif start_story == 'n':
+        print(f"Goodbye, {users_name}")
+    else: 
+        print("Please select either y/n.\n")
+    return start_story
+
+users_name = greetings()
+new_story(users_name)
