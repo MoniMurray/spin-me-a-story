63af8eb13a4a11b581fbc879c27fcd95b6322245
Monica Murray <Monica.rickerby@gmail.com>
2022-11-19 12:53:38 +0000

    Move ternary expression outside of while loop to avoid greeting being repeated on exception running

--- run.py
+++ run.py
@@ -8,8 +8,9 @@ def greetings():
     return user_name
 
 def new_story(users_name):
+    print(f"Would you like to write a story, {users_name}?")
     while True:
-        start_story = input(f"Would you like to write a story, {users_name}?\nSelect 'y for yes, or 'n' to exit.\ny/n \n")
+        start_story = input(f"Select 'y for yes, or 'n' to exit.\ny/n \n")
 
         if start_story == 'y':
             print("Once Upon a Time...")
@@ -28,9 +29,9 @@ def user_continue(start_story, users_name):
     try:
         if start_story != 'y':
             if start_story != 'n':
-                raise ValueError(f"Only y/n can be accepted, you typed {start_story}")
+                raise ValueError()
     except ValueError as e:
-        print(f"ValueError: Please select either y/n.\n")
+        print(f"ValueError: Please select either y/n {users_name}.\n")
         return False
     
     return True
