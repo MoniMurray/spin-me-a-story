faea891852c86f47722b98033df23d91f53d8fb8
Monica Murray <Monica.rickerby@gmail.com>
2022-11-20 12:47:55 +0000

    Move story components into a Class to call for user to build their story

--- run.py
+++ run.py
@@ -9,6 +9,78 @@ def greetings():
     user_name = input("Hello!  What is your name?\n")
     return user_name
 
+class Story_bones:
+    """
+    Define story elements to be called using random to build stories
+    """
+    def __init__(self, character, beginning, time_of_day, animal, dice_1, 
+    dice_2, dice_3, dice_4, verb, users_name):
+        self.character = character
+        self.beginning = beginning
+        self.time_of_day = time_of_day
+        self.animal = animal
+        self.dice_1 = dice_1
+        self.dice_2 = dice_2
+        self.dice_3 = dice_3
+        self.dice_4 = dice_4
+        self.verb = verb
+
+    def story_bricks (self):
+        character = ["Aoife", "Shauna", "Eoin", "Millie", "Evie", "Lauren"]
+        beginning = [
+            "One fine morning", 
+            "It was dark and stormy", 
+            "The sun shone brightly",
+            "The birds were singing",
+            "I couldn't believe my eyes"]
+        time_of_day = ["yesterday", "evening", "tomorrow", "last night"]
+        animal = ["a cat", "a rat", "a badger", "a bee", "a bird", "a paraqueet"]
+        dice_1 = [
+            "curious brown monkey", 
+            "shiny chalice", 
+            "speedometer", 
+            "rising sun", 
+            "red backpack", 
+            "prickly cactus"]
+        dice_2 = [
+            "peanuts", 
+            "dinosaur bones", 
+            "a bag of gold", 
+            "an igloo", 
+            "a spider", 
+            "a caterpiller"]
+        dice_3 = [
+            "jigsaw", 
+            "mountain peak", 
+            "pirate's treasure chest", 
+            "precious eagle egg", 
+            "circus tent", 
+            "elephant"]
+        dice_4 = [
+            "music", 
+            "blackbird", 
+            "confusion", 
+            "an axe", 
+            "a ladder", 
+            "a telescope"]
+        verb = ["singing", 
+        "dancing", 
+        "walking", 
+        "listening", 
+        "digging", 
+        "cycling", 
+        "skating", 
+        "shouting", 
+        "painting"]
+       
+        for i in character:
+            for i in beginning:
+                for i in verb:
+                    return f"{random.choice(beginning)}, {random.choice(character)} was {random.choice(verb)}"
+# opening_paragraph = Story_bones(random.choice(self.beginning) + ", " + f"{users_name}" + " and a friend, " + random.choice(self.character) + " were " + random.choice(self.verb) + "...\n\n")
+# print(opening_paragraph)
+    
+
 def new_story(users_name):
     print(f"Would you like to write a story, {users_name}?")
     while True:
@@ -18,7 +90,7 @@ def new_story(users_name):
             print("\nThen let's begin...\n")
             break
         elif start_story == 'n':
-            # print(f"Goodbye, {users_name}")
+            print(f"Goodbye, {users_name}")
             sys.exit()
             break
         
@@ -39,60 +111,60 @@ def user_continue(start_story, users_name):
     
     return True
 
-def story_bones(users_name):
-    """
-    Define story elements to be called using random to build stories
-    """
-    character = ["Aoife", "Shauna", "Eoin", "Millie", "Evie", "Lauren"]
-    beginning = [
-        "One fine morning", 
-        "It was dark and stormy", 
-        "The sun shone brightly",
-        "The birds were singing",
-        "I couldn't believe my eyes"]
-    time_of_day = ["yesterday", "evening", "tomorrow", "last night"]
-    animal = ["a cat", "a rat", "a badger", "a bee", "a bird", "a paraqueet"]
-    dice_1 = [
-        "curious brown monkey", 
-        "shiny chalice", 
-        "speedometer", 
-        "rising sun", 
-        "red backpack", 
-        "prickly cactus"]
-    dice_2 = [
-        "peanuts", 
-        "dinosaur bones", 
-        "a bag of gold", 
-        "an igloo", 
-        "a spider", 
-        "a caterpiller"]
-    dice_3 = [
-        "jigsaw", 
-        "mountain peak", 
-        "pirate's treasure chest", 
-        "precious eagle egg", 
-        "circus tent", 
-        "elephant"]
-    dice_4 = [
-        "music", 
-        "blackbird", 
-        "confusion", 
-        "an axe", 
-        "a ladder", 
-        "a telescope"]
-    verb = ["singing", 
-    "dancing", 
-    "walking", 
-    "listening", 
-    "digging", 
-    "cycling", 
-    "skating", 
-    "shouting", 
-    "painting"]
+# def story_bones(users_name):
+#     """
+#     Define story elements to be called using random to build stories
+#     """
+#     character = ["Aoife", "Shauna", "Eoin", "Millie", "Evie", "Lauren"]
+#     beginning = [
+#         "One fine morning", 
+#         "It was dark and stormy", 
+#         "The sun shone brightly",
+#         "The birds were singing",
+#         "I couldn't believe my eyes"]
+#     time_of_day = ["yesterday", "evening", "tomorrow", "last night"]
+#     animal = ["a cat", "a rat", "a badger", "a bee", "a bird", "a paraqueet"]
+#     dice_1 = [
+#         "curious brown monkey", 
+#         "shiny chalice", 
+#         "speedometer", 
+#         "rising sun", 
+#         "red backpack", 
+#         "prickly cactus"]
+#     dice_2 = [
+#         "peanuts", 
+#         "dinosaur bones", 
+#         "a bag of gold", 
+#         "an igloo", 
+#         "a spider", 
+#         "a caterpiller"]
+#     dice_3 = [
+#         "jigsaw", 
+#         "mountain peak", 
+#         "pirate's treasure chest", 
+#         "precious eagle egg", 
+#         "circus tent", 
+#         "elephant"]
+#     dice_4 = [
+#         "music", 
+#         "blackbird", 
+#         "confusion", 
+#         "an axe", 
+#         "a ladder", 
+#         "a telescope"]
+#     verb = ["singing", 
+#     "dancing", 
+#     "walking", 
+#     "listening", 
+#     "digging", 
+#     "cycling", 
+#     "skating", 
+#     "shouting", 
+#     "painting"]
 
-    opening_paragraph = random.choice(beginning) + ", " + f"{users_name}" + " and a friend, " + random.choice(character) + " were " + random.choice(verb) + "...\n\n"
-    print(opening_paragraph)
-    # return opening_paragraph
+#     opening_paragraph = random.choice(beginning) + ", " + f"{users_name}" + " and a friend, " + random.choice(character) + " were " + random.choice(verb) + "...\n\n"
+#     print(opening_paragraph)
+#     # return opening_paragraph
 
 # def user_choose_path(story_options, users_name):
 #     possibility_1 = f"{users_name}" + "'s pet," + random.choice(animal)
@@ -104,7 +176,9 @@ def main():
     
     users_name = greetings()
     start_new_story = new_story(users_name)
-    story_options = story_bones(users_name)
+    # story_options = story_bones(users_name)
     # first_choice = user_choose_path(story_options, users_name)
+    opening_paragraph = Story_bones('character', 'beginning', 'time_of_day', 'animal', 'dice_1', 'dice_2', 'dice_3', 'dice_4', 'verb','users_name')
+    print(opening_paragraph.story_bricks())
 
 main()
