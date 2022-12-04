7a88506a74de7df873f6a19af7a3a6911e425612
Monica Murray <Monica.rickerby@gmail.com>
2022-12-04 13:54:03 +0000

    Address final gitpod problems and test

--- run.py
+++ run.py
@@ -25,7 +25,7 @@ def greetings():
     return user_name
 
 
-class Story_bones:
+class Storybones:
     """
     Define story elements to be called using random.choice() to build
     stories.  Lists containing components for a story will be
@@ -136,15 +136,10 @@ class Story_bones:
         """
         Generate random short-story opening line.
         """
-        """first_line = [(i, j, k, m)
-                      for i in Story_bones.character
-                      for j in Story_bones.beginning
-                      for k in Story_bones.verb
-                      for m in Story_bones.dice_3]"""
-        opening = (f"\n{random.choice(Story_bones.beginning)},"
-                   f" {random.choice(Story_bones.character)}"
-                   f" {random.choice(Story_bones.verb)}\n"
-                   f"a {random.choice(Story_bones.dice_3)}"
+        opening = (f"\n{random.choice(Storybones.beginning)},"
+                   f" {random.choice(Storybones.character)}"
+                   f" {random.choice(Storybones.verb)}\n"
+                   f"a {random.choice(Storybones.dice_3)}"
                    f" when...\n")
         return opening
 
@@ -153,9 +148,9 @@ class Story_bones:
         Generate random first option path to continue short-story.
         """
         path_1 = (
-                f"1: ...{random.choice(Story_bones.animal)}"
+                f"1: ...{random.choice(Storybones.animal)}"
                 f" named {users_name}, carrying"
-                f" {random.choice(Story_bones.dice_2)},...\n")
+                f" {random.choice(Storybones.dice_2)},...\n")
         return path_1
 
     def possibility_2(self, users_name):
@@ -163,11 +158,11 @@ class Story_bones:
         Generate random second option path to continue short-story.
         """
         path_2 = (f"2: ...I remembered that"
-                  f" {random.choice(Story_bones.time_of_day)}"
+                  f" {random.choice(Storybones.time_of_day)}"
                   f" {users_name}'s pet,\n"
-                  f"     {random.choice(Story_bones.animal)}"
+                  f"     {random.choice(Storybones.animal)}"
                   f" named Puddles, spotted"
-                  f" {random.choice(Story_bones.dice_2)}"
+                  f" {random.choice(Storybones.dice_2)}"
                   f" and...\n")
         return path_2
 
@@ -176,8 +171,8 @@ class Story_bones:
         Generate random third option path to continue short-story.
         """
         path_3 = (f"3: ...suddenly,"
-                  f" {random.choice(Story_bones.animal)} said,"
-                  f" {random.choice(Story_bones.quote)}...\n")
+                  f" {random.choice(Storybones.animal)} said,"
+                  f" {random.choice(Storybones.quote)}...\n")
         return path_3
 
     def options(self):
@@ -189,41 +184,41 @@ class Story_bones:
             print("*" * 80)
             if user_choice == '1':
                 story = (f"\nThe story continues:\n\n...began to tell"
-                         f" a story.\n{random.choice(Story_bones.dice_5)}"
+                         f" a story.\n{random.choice(Storybones.dice_5)}"
                          f" saw\na very large crowd of people"
                          f" gathered outside the gates\nof the local"
                          f" carnival, where"
-                         f" {random.choice(Story_bones.dice_6)}"
+                         f" {random.choice(Storybones.dice_6)}"
                          f" was\ncoming to perform their show.\n"
                          f"A silvery"
                          f" voice behind them said"
-                         f"\n{random.choice(Story_bones.quote)},\n"
+                         f"\n{random.choice(Storybones.quote)},\n"
                          f"which made no sense whatsoever!\n"
                          f"It had all been a dream!!\n")
                 break
             elif user_choice == '2':
                 story = (f"\nThe story continues:\n\n..."
-                         f"{random.choice(Story_bones.dice_5)}"
+                         f"{random.choice(Storybones.dice_5)}"
                          f" said:\n'I'm so glad to see you, please"
                          f" stay for tea.'  Afterwards,"
                          f" when\neveryone was full, "
-                         f"{random.choice(Story_bones.dice_6)}"
+                         f"{random.choice(Storybones.dice_6)}"
                          f" danced and sang and performed a"
                          f" wonderful show.\nAfter seeing that, it"
                          f" seemed like a sensible idea to play with"
-                         f"\n{random.choice(Story_bones.animal)},"
+                         f"\n{random.choice(Storybones.animal)},"
                          f" go home, have a nap, and plan for"
                          f" tomorrow's great adventure.\nAnd oh, "
                          f" what an adventure that was going to be!\n ")
                 break
             elif user_choice == '3':
                 story = (f"\nThe story continues:...\n\n"
-                         f"{random.choice(Story_bones.dice_5)}"
+                         f"{random.choice(Storybones.dice_5)}"
                          f"\nwhose "
-                         f"{random.choice(Story_bones.dice_4)}"
+                         f"{random.choice(Storybones.dice_4)}"
                          f" was magical too,\n"
                          f"showed photographs of"
-                         f" {random.choice(Story_bones.animal)}"
+                         f" {random.choice(Storybones.animal)}"
                          f" riding his first bicycle,\non a"
                          f" carousel at the fair,\nplaying a"
                          f" computer game with his father,\n"
@@ -232,7 +227,7 @@ class Story_bones:
                          f"there was a mystery - because"
                          f" the house held no\nsign at all"
                          f" that"
-                         f" {random.choice(Story_bones.animal)}"
+                         f" {random.choice(Storybones.animal)}"
                          f"-boy lived there too!\n")
                 break
             elif user_options(user_choice):
@@ -328,7 +323,7 @@ def main():
     users_name = greetings()
     selection = new_story(users_name)
     print("*" * 80)
-    opening_paragraph = Story_bones(
+    opening_paragraph = Storybones(
         'character', 'beginning', 'time_of_day', 'animal',
         'dice_1', 'dice_2', 'dice_3', 'dice_4', 'dice_5', 'dice_6',
         'verb', 'quote', 'users_name')
@@ -337,22 +332,22 @@ def main():
     sleep(4.0)
     print("\nChoose from one of the following three path options to"
           " continue your story:\n")
-    option_1 = Story_bones(
+    option_1 = Storybones(
         'character', 'beginning', 'time_of_day', 'animal', 'dice_1',
         'dice_2', 'dice_3', 'dice_4', 'dice_5', 'dice_6', 'verb',
         'quote', 'users_name')
     print(option_1.possibility_1(users_name))
-    option_2 = Story_bones(
+    option_2 = Storybones(
         'character', 'beginning', 'time_of_day', 'animal', 'dice_1',
         'dice_2', 'dice_3', 'dice_4', 'dice_5', 'dice_6', 'verb',
         'quote', 'users_name')
     print(option_2.possibility_2(users_name))
-    option_3 = Story_bones(
+    option_3 = Storybones(
         'character', 'beginning', 'time_of_day', 'animal', 'dice_1',
         'dice_2', 'dice_3', 'dice_4', 'dice_5', 'dice_6', 'verb',
         'quote', 'users_name')
     print(option_3.possibility_3())
-    userchoices = Story_bones(
+    userchoices = Storybones(
             'character', 'beginning', 'time_of_day', 'animal', 'dice_1',
             'dice_2', 'dice_3', 'dice_4', 'verb', 'quote', 'dice_5', 'dice_6',
             'users_name')
@@ -364,7 +359,7 @@ def main():
         os.system('cls' if os.name == 'nt' else 'clear')
         print(f"\n{users_name}'s New Story\n")
         print("*" * 80)
-        opening_paragraph = Story_bones(
+        opening_paragraph = Storybones(
             'character', 'beginning', 'time_of_day', 'animal',
             'dice_1', 'dice_2', 'dice_3', 'dice_4', 'dice_5', 'dice_6',
             'verb', 'quote', 'users_name')
@@ -373,23 +368,23 @@ def main():
         sleep(4.0)
         print("\nChoose one of the following three path options"
               " to continue your story:\n")
-        option_1 = Story_bones(
+        option_1 = Storybones(
             'character', 'beginning', 'time_of_day', 'animal', 'dice_1',
             'dice_2', 'dice_3', 'dice_4', 'dice_5', 'dice_6', 'verb',
             'quote', 'users_name')
         print(option_1.possibility_1(users_name))
-        option_2 = Story_bones(
+        option_2 = Storybones(
             'character', 'beginning', 'time_of_day', 'animal', 'dice_1',
             'dice_2', 'dice_3', 'dice_4', 'dice_5', 'dice_6', 'verb',
             'quote', 'users_name')
         print(option_2.possibility_2(users_name))
-        option_3 = Story_bones(
+        option_3 = Storybones(
             'character', 'beginning', 'time_of_day', 'animal', 'dice_1',
             'dice_2', 'dice_3', 'dice_4', 'dice_5', 'dice_6', 'verb',
             'quote', 'users_name')
         print(option_3.possibility_3())
         print("*" * 80)
-        userchoices = Story_bones(
+        userchoices = Storybones(
             'character', 'beginning', 'time_of_day', 'animal', 'dice_1',
             'dice_2', 'dice_3', 'dice_4', 'verb', 'quote', 'dice_5', 'dice_6',
             'users_name')
@@ -398,10 +393,10 @@ def main():
         sleep(10.0)
 
 
-title = "'Spin Me A Story' - an interactive, random story generator\n"
-x = title.center(80)
+TITLE = "'Spin Me A Story' - an interactive, random story generator\n"
+X = TITLE.center(80)
 print("*" * 80)
-print(x)
+print(X)
 print("*" * 80)
 
 main()
